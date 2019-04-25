b2947c3af777b266685d8dfc3f9d160b48077c84
meddlin <drushing.dev@gmail.com>
2019-04-24 21:43:56 -0700

    List CPU info script. Playing with loops.

---
+++ cpu.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "CPU Info Listing"
+
+echo "Running: cpuinfo"
+cat /proc/cpuinfo
+
+echo "Running: lscpu"
+lscpu
---
+++ create-dirs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Create & Crawl Directories"
+
+for i in {0..2} # Run through a loop 3 times (0-based index)
+do
+	echo "Number: $i"
+done
