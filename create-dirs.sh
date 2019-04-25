2579c4d27101d6beaf26060880cf8eaa2bcb1e62
meddlin <drushing.dev@gmail.com>
2019-04-24 22:14:03 -0700

    create-dirs.sh now creates, lists, and removes the directories

--- create-dirs.sh
+++ create-dirs.sh
@@ -1,11 +1,29 @@
 #!/bin/bash
-echo "Create & Crawl Directories"
+echo "Create & Delete Directories"
 
 ## Create directories
 dirname=""
+dirs=()
 for i in {0..2}
 do
 	dirname="dir-"$i # Create new directory name with index
 	echo "Creating... /$dirname"
 	mkdir $dirname
+	dirs+=( $dirname )
 done
+
+## List the directories created
+echo "dirs created: "
+for d in ${dirs[@]}; do
+	echo $d
+done
+
+## Remove the directories we created
+echo "dirs removal: "
+for d in ${dirs[@]}; do
+	echo "Removing! $d"
+	rm -r $d
+done
+
+## List the current directory after things are cleaned up
+ls -al
